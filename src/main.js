143b56805c264b5e5e255fcdf15c8ab4ff84fe30
Aries Cs <aries0d0f@gmail.com>
2018-07-05 16:33:05 +0800

    Fix FontAwesome

--- src/main.js
+++ src/main.js
@@ -1,20 +1,20 @@
-import Vue              from 'vue'
-import axios            from 'axios'
-import VueAxios         from 'vue-axios'
-import { sync }         from 'vuex-router-sync'
+import Vue                  from 'vue'
+import axios                from 'axios'
+import VueAxios             from 'vue-axios'
+import { sync }             from 'vuex-router-sync'
 
 // Fontawesome
 // the component
-import FontAwesomeIcon  from '@fortawesome/vue-fontawesome'
+import { FontAwesomeIcon }  from '@fortawesome/vue-fontawesome'
 // the library
-import fontawesome      from '@fortawesome/fontawesome'
+import { library }          from '@fortawesome/fontawesome-svg-core'
 // add more icon categories as you want them, even works with pro packs
-import brands           from '@fortawesome/fontawesome-free-brands'
-import solid            from '@fortawesome/fontawesome-free-solid'
+import { fab }              from '@fortawesome/free-brands-svg-icons'
+import { fas }              from '@fortawesome/free-solid-svg-icons'
 
-import App              from './App'
-import router           from './router'
-import store            from './store'
+import App                  from './App'
+import router               from './router'
+import store                from './store'
 
 Vue.router = router
 Vue.use(VueAxios, axios)
@@ -24,11 +24,12 @@ sync(store, router)
 Vue.config.devtools = true
 
 // asociate it to the library, if you need to add more you can separate them by a comma
-fontawesome.library.add(brands)
-fontawesome.library.add(solid)
-Vue.component('FontAwesomeIcon', FontAwesomeIcon)
+library.add(fab)
+library.add(fas)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 const app = new Vue({
+  store,
   router,
   ...App
 }).$mount('#app')
